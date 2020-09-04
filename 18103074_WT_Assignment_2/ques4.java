0ee3b4b3df1d532631ae08bdc1d371d37a367fad
akshitm169 <54849887+akshitm169@users.noreply.github.com>
2020-09-04 23:14:12 +0530

    Add files via upload

---
+++ 18103074_WT_Assignment_2/ques4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum=0, i=1;
+        for(; sum<Integer.MAX_VALUE && i<Integer.MAX_VALUE; i++) {
+            if(sum+i>=Integer.MAX_VALUE)
+                break;
+            sum+=i;
+            if(sum==i*i)
+                System.out.println(i);
+        }
+    }
+}
